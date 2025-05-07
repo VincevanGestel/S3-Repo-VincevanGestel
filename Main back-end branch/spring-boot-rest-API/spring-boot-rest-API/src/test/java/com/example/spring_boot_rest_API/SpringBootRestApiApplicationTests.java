662686856a86caed1232965a841bb34461a85e18
Vince van Gestel <v.vangestel@student.fontys.nl>
2025-05-07 14:11:17 +0200

    Added h2 database to prevent pipeline errors on tests, and help with tests later on

--- Main back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/SpringBootRestApiApplicationTests.java
+++ Main back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/SpringBootRestApiApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.spring_boot_rest_API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootRestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Testing context load...");
 	}
 
 }
