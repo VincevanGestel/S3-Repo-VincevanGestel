f272c97c9319afc7794b341f6b18885e0985a108
VincevanGestel <161011639+VincevanGestel@users.noreply.github.com>
2025-05-23 18:33:43 +0200

    Update ProductControllerIntegrationTest.java

--- Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/intergration/ProductControllerIntegrationTest.java
+++ Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/intergration/ProductControllerIntegrationTest.java
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class ProductControllerIntegrationTest {
 
     @Autowired private MockMvc mockMvc;
