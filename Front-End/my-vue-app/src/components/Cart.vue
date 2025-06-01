691e9fc4b80de4261387b37b065ac81a055b4475
Vince van Gestel <v.vangestel@student.fontys.nl>
2025-06-01 14:40:13 +0200

    Remove Typescript, If I want to use typescript in my components I'll have to rewrite my api.js file, change it to a .ts file and rewrite my components

--- Front-End/my-vue-app/src/components/Cart.vue
+++ Front-End/my-vue-app/src/components/Cart.vue
@@ -1,50 +1,16 @@
-<template>
-  <div>
-    <h2>Your Cart</h2>
-    <div v-if="cart?.items?.length">
-      <ul>
-        <li v-for="item in cart.items" :key="item.product.id">
-          {{ item.product.name }} -
-          €{{ (item.product.price * item.quantity).toFixed(2) }}
-
-          <!-- Quantity input -->
-          <input
-            type="number"
-            min="1"
-            v-model.number="item.quantity"
-            @change="updateQuantity(item.product.id, item.quantity)"
-            style="width: 50px; margin-left: 10px;"
-          />
-
-          <button @click="remove(item.product.id)" style="margin-left: 10px;">
-            Remove
-          </button>
-        </li>
-      </ul>
-
-      <p><strong>Total:</strong> €{{ total.toFixed(2) }}</p>
-      <button @click="clear">Clear Cart</button>
-    </div>
-
-    <div v-else>
-      <p>Your cart is empty.</p>
-    </div>
-  </div>
-</template>
-
-<script lang="ts">
+<script>
 import { computed, defineComponent, onMounted, ref } from 'vue';
 import { getCart, removeFromCart, clearCart, updateCartQuantity } from '../services/api';
 
 export default defineComponent({
   setup() {
-    const cart = ref<any>(null);
+    const cart = ref(null);
 
     const fetchCart = async () => {
       cart.value = await getCart();
     };
 
-    const remove = async (productId: number) => {
+    const remove = async (productId) => {
       await removeFromCart(productId);
       await fetchCart();
     };
@@ -54,9 +20,8 @@ export default defineComponent({
       await fetchCart();
     };
 
-    const updateQuantity = async (productId: number, quantity: number) => {
+    const updateQuantity = async (productId, quantity) => {
       if (quantity < 1) {
-        // Optional: prevent setting quantity less than 1 or remove item
         await remove(productId);
       } else {
         await updateCartQuantity(productId, quantity);
@@ -65,7 +30,7 @@ export default defineComponent({
     };
 
     const total = computed(() => {
-      return cart.value?.items?.reduce((sum: number, item: any) => {
+      return cart.value?.items?.reduce((sum, item) => {
         return sum + item.product.price * item.quantity;
       }, 0) || 0;
     });
