4a59bce57d76fa01b1d9dd6d35312bcba48cf43a
Vince van Gestel <v.vangestel@student.fontys.nl>
2025-06-04 14:13:18 +0200

    fixed pipeline error

--- Front-End/my-vue-app/src/stores/auth.ts
+++ Front-End/my-vue-app/src/stores/auth.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import api from '@/services/axiosInstance';
+import api from '@/plugins/axios';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
