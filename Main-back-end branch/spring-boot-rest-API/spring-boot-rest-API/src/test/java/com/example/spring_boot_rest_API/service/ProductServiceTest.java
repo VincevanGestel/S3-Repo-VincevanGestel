29ae8a046e161bcff7bf9a049c8c0dcdac69e186
Vince van Gestel <v.vangestel@student.fontys.nl>
2025-06-13 13:47:37 +0200

    Properly apply regression testing (pushing no images to docker if tests fail) and removed failing test

--- Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/service/ProductServiceTest.java
+++ Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/service/ProductServiceTest.java
@@ -61,11 +61,6 @@ public class ProductServiceTest {
     }
 
     @Test
-    void failingTest()
-    {
-        assertEquals(1,0);
-    }
-    @Test
     void testSaveProduct() {
         when(tagRepository.findAllById(tagIds)).thenReturn(tagList);
         when(productRepository.save(any(Product.class))).thenReturn(product);
