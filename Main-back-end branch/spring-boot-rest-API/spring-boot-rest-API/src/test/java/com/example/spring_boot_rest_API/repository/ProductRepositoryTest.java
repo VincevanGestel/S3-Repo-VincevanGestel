a43d2b110a371b192e2ed3007f232849a6f5aa80
Vince van Gestel <v.vangestel@student.fontys.nl>
2025-06-20 09:57:55 +0200

    added @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) To prevent spring from overwriting the setup I have in my classes (uses test database)

--- Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/repository/ProductRepositoryTest.java
+++ Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/repository/ProductRepositoryTest.java
@@ -4,7 +4,9 @@ import com.example.spring_boot_rest_API.model.Product;
 import com.example.spring_boot_rest_API.model.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProductRepositoryTest {
 
     @Autowired
--- Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/repository/TagRepositoryTest.java
+++ Main-back-end branch/spring-boot-rest-API/spring-boot-rest-API/src/test/java/com/example/spring_boot_rest_API/repository/TagRepositoryTest.java
@@ -1,21 +1,31 @@
 package com.example.spring_boot_rest_API.repository;
 
 import com.example.spring_boot_rest_API.model.Tag;
-import com.example.spring_boot_rest_API.repository.TagRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@EntityScan("com.example.spring_boot_rest_API.model")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class TagRepositoryTest {
-    @Mock
+
+    @Autowired
     private TagRepository tagRepository;
 
     @Test
     void testSaveTag() {
-        Tag tag = new Tag();
-        tag.setName("TestTag");
+        Tag tag = new Tag("TestTag");
         tagRepository.save(tag);
+
+        List<Tag> tags = tagRepository.findByNameIn(List.of("TestTag"));
+        assertThat(tags).hasSize(1);
+        assertThat(tags.get(0).getName()).isEqualTo("TestTag");
     }
 }
